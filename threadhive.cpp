676b496663296f0908437e75595a011db760b1b8
Ilya Patrzykat <ilyapatrzykat@dhcp-128-189-78-123.ubcsecure.wireless.ubc.ca>
2023-05-22 11:43:46 -0700

    1.2.0 get return value from worker threads

--- threadhive.cpp
+++ threadhive.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <pthread.h>
 #include <chrono>
+#include <cstdlib>
+#include <ctime>
 
 class ThreadHive {
 public:
@@ -78,30 +80,69 @@ private:
 };
 
 void do_work() {
-    for (int i = 0; i < 500000000; ++i) {}  // simulate work by running an empty loop
-    std::cout << "Done a unit of work\n";
+    for (int i = 0; i < 200000000; ++i) {}
+    // std::cout << "Done a unit of work\n";
 }
 
-int main() {
+void do_work_with_return(int &output) {
+    std::srand(std::time(nullptr));
+    int num = std::rand();
+    for (int i = 0; i < 200000000; ++i) {}  // simulate work by running an empty loop
+    // std::cout << "Done a unit of work\n";
+    output = num;
+}
+
+void test_single_thread() {
     std::cout << "Single thread: start\n";
     auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 10; ++i) do_work();
+    for (int i = 0; i < 12; ++i) do_work();
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
     std::cout << "Single thread: done in " << elapsed.count() << " seconds.\n";
-    
+    std::cout << "----------------------------------------\n";
+}
+
+void test_thread_pool() {
     std::cout << "Thread pool: start\n";
+    auto start = std::chrono::high_resolution_clock::now();
     // ThreadHive destructor will automatically be called when the object goes out of scope
     // Explicitly calling destructor caused double free error
     {
         ThreadHive pool(1);
         pool.resize(4);
-        start = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < 12; ++i) pool.enqueue(do_work);
     }
-    end = std::chrono::high_resolution_clock::now();
-    elapsed = end - start;
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
     std::cout << "Thread pool: done in " << elapsed.count() << " seconds.\n";
+    std::cout << "----------------------------------------\n";
+}
+
+void test_thread_pool_with_return() {
+    std::cout << "Thread pool with return: start\n";
+    auto start = std::chrono::high_resolution_clock::now();
+    std::vector<int> results(12);
+    // ThreadHive destructor will automatically be called when the object goes out of scope
+    // Explicitly calling destructor caused double free error
+    {
+        ThreadHive pool(4);
+        for (int i = 0; i < 12; ++i) pool.enqueue([&results, i](){ do_work_with_return(results[i]); });
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Thread pool with return: done in " << elapsed.count() << " seconds.\n";
+
+    std::cout << "Results:\n";
+    for (int i = 0; i < 12; ++i) std::cout << results[i] << " ";
+    std::cout << "\n";
+    std::cout << "----------------------------------------\n";
+}
+
+int main() {
+
+    test_single_thread();
+    test_thread_pool();
+    test_thread_pool_with_return();
 
     return 0;
 }
